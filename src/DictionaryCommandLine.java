dc9434941c38aa49d6641769c1aad98ab2fe9df6
Huong <20020423@vnu.edu.vn>
2021-09-30 21:37:35 +0700

    add dictionarySearcher()

--- src/DictionaryCommandLine.java
+++ src/DictionaryCommandLine.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DictionaryCommandLine extends DictionaryManagement{
 
     /** Print all the words in the given format (ver 1). */
@@ -12,7 +14,50 @@ public class DictionaryCommandLine extends DictionaryManagement{
 
     /** ver 1: Hàm dictionaryBasic() gọi 2 hàm: insertFromCommandline() và showAllWords(). */
     public void dictionaryBasic() {
-        insertFromCommandLine();
+        //insertFromCommandLine();
         showAllWords();
+    }
+
+    /** Tìm vị trí từ đầu tiên chứa xâu s ở đầu. */
+    public int findMinIndex(String s){
+        int i = -1;
+        int left = 0;
+        int right = getListWord().size() - 1;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (getListWord().get(mid).getWord_target().startsWith(s)) {
+                i = mid;
+                break;
+            } else {
+                if (getListWord().get(mid).getWord_target().compareTo(s) > 0) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        if (i != -1) {
+            while (i > 0 && getListWord().get(i - 1).getWord_target().startsWith(s)){
+                i--;
+            }
+        }
+        return i;
+    }
+
+    /** Ver 3: tìm kiếm từ bắt đầu từ xâu cho trước. */
+    void dictionarySearcher() {
+        String s;
+        Scanner sc = new Scanner(System.in);
+        s = sc.next();
+        int i = findMinIndex(s);
+        if (i == -1) {
+            System.out.println("Not found.");
+        } else {
+            while (i < getListWord().size() && getListWord().get(i).getWord_target().startsWith(s)) {
+                System.out.printf("%-20s%s\n", getListWord().get(i).getWord_target(), getListWord().get(i).getWord_explain());
+                i++;
+            }
+        }
     }
 }
